228d23f6710210edbe9cc6a9ed81783fa8cab392
maxiamikel <maxloversist@gmail.com>
2022-12-30 10:42:05 -0300

    Inicializar o baek end, criando o projeto

---
+++ backservico/src/main/java/com/maxi/backservico/BackservicoApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackservicoApplication.class, args);
+	}
+
+}
